aaa01429d5087da03cffb654832b662d6b86ace9
mako <mako@example.com>
2026-03-08 15:59:55 +0900

    chore: catch middleware execution error

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,9 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import { NextResponse } from "next/server";
 
-let auth: any;
-try {
-    const authResult = NextAuth(authConfig);
-    auth = authResult.auth;
-} catch (error: any) {
-    auth = (req: any) => {
-        return NextResponse.json({ error: "Middleware Init Error: " + error.message, stack: error.stack }, { status: 500 });
-    };
-}
+const { auth } = NextAuth(authConfig);
 
-export default auth((req: any) => {
+const middleware = auth((req) => {
     const { pathname } = req.nextUrl;
 
     // Allow public routes
@@ -28,6 +20,15 @@ export default auth((req: any) => {
 
     return NextResponse.next();
 });
+
+export default async function customMiddleware(req: any, ev: any) {
+    try {
+        // @ts-ignore
+        return await middleware(req, ev);
+    } catch (error: any) {
+        return NextResponse.json({ error: "Middleware Exec Error: " + error.message, stack: error.stack }, { status: 500 });
+    }
+}
 
 export const config = {
     matcher: ["/((?!_next/static|_next/image|favicon.ico|icons|manifest).*)"],
